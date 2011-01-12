0f987da6536664b8ca9f9d4fff001d2f4b871bb1
matthias.weidlich <matthias.weidlich@1f0ec036-ed87-11dd-a45c-2d54f07c7ce3>
2011-01-12 15:51:58 +0000

    test for dominator / postdominator algo

--- src/de/hpi/bpt/graph/test/DirectedGraphTest.java
+++ src/de/hpi/bpt/graph/test/DirectedGraphTest.java
@@ -21,6 +21,9 @@
  */
 package de.hpi.bpt.graph.test;
 
+import java.util.Map;
+import java.util.Set;
+
 import junit.framework.TestCase;
 import de.hpi.bpt.graph.DirectedEdge;
 import de.hpi.bpt.graph.DirectedGraph;
@@ -53,6 +56,23 @@ public class DirectedGraphTest extends TestCase {
 		assertEquals(0,g.getDisconnectedVertices().size());
 		assertEquals(4,g.getConnectedVertices().size());
 		
+		Map<Vertex,Set<Vertex>> dom = dga.getDominators(g, false);
+		Map<Vertex,Set<Vertex>> pdom = dga.getDominators(g, true);
+		
+		assertEquals(4,dom.keySet().size());
+		assertEquals(4,pdom.keySet().size());
+		
+		assertEquals(1,dom.get(v1).size());
+		assertTrue(dom.get(v1).contains(v1));
+
+		assertEquals(2,pdom.get(v1).size());
+		assertTrue(pdom.get(v1).contains(v1));
+		assertTrue(pdom.get(v1).contains(v2));
+
+		assertEquals(2,dom.get(v2).size());
+		assertTrue(dom.get(v2).contains(v1));
+		assertTrue(dom.get(v2).contains(v2));
+		
 		assertNotNull(g.addVertex(v5));
 		assertEquals(1,g.getDisconnectedVertices().size());
 		assertEquals(4,g.getConnectedVertices().size());
@@ -64,10 +84,41 @@ public class DirectedGraphTest extends TestCase {
 		assertEquals(0, g.getPredecessors(v1).size());
 		assertEquals(1, g.getSuccessors(v1).size());
 		
+		DirectedEdge e4 = g.addEdge(v3, v5);
+		DirectedEdge e5 = g.addEdge(v4, v5);
 		
+		dom = dga.getDominators(g, false);
+		pdom = dga.getDominators(g, true);
 		
+		assertEquals(5,dom.keySet().size());
+		assertEquals(5,pdom.keySet().size());
 		
+		assertEquals(3,dom.get(v5).size());
+		assertTrue(dom.get(v5).contains(v1));
+		assertTrue(dom.get(v5).contains(v2));
+		assertTrue(dom.get(v5).contains(v5));
+
+		assertEquals(2,pdom.get(v4).size());
+		assertTrue(pdom.get(v4).contains(v4));
+		assertTrue(pdom.get(v4).contains(v5));
+
+		assertNotNull(g.addVertex(v6));
 		
+		DirectedEdge e6 = g.addEdge(v6, v4);
+		
+		dom = dga.getDominators(g, false);
+		pdom = dga.getDominators(g, true);
+		
+		assertEquals(6,dom.keySet().size());
+		assertEquals(6,pdom.keySet().size());
+		
+		assertEquals(1,dom.get(v5).size());
+		assertTrue(dom.get(v5).contains(v5));
+
+		assertEquals(3,pdom.get(v6).size());
+		assertTrue(pdom.get(v6).contains(v4));
+		assertTrue(pdom.get(v6).contains(v4));
+		assertTrue(pdom.get(v6).contains(v6));
 		
 		
 		
