c53021111c25807dbd58f523cbec7970cecd6b97
artem.polyvyanyy <artem.polyvyanyy@1f0ec036-ed87-11dd-a45c-2d54f07c7ce3>
2011-05-10 17:46:52 +0000

    minor fixes for Process

--- src/de/hpi/bpt/process/serialize/Process2DOT.java
+++ src/de/hpi/bpt/process/serialize/Process2DOT.java
@@ -17,20 +17,24 @@ public class Process2DOT {
 		String result = "";
 		if (process == null) return result;
 		
-		result += "digraph G {";
+		result += "digraph G {\n";
 		for (Task t : process.getTasks())
-			result += String.format("\tn%s[shape=box,label=\"%s\"];\n", t.getId().replace("-", ""), t.getName());
+			result += String.format("  n%s[shape=box,label=\"%s\"];\n", t.getId().replace("-", ""), t.getName());
+		result+="\n";
 		for (Gateway g : process.getGateways(GatewayType.AND))
-			result += String.format("\tn%s[shape=diamond,label=\"%s\"];\n", g.getId().replace("-", ""), "AND");
+			result += String.format("  n%s[shape=diamond,label=\"%s\"];\n", g.getId().replace("-", ""), "AND");
 		for (Gateway g : process.getGateways(GatewayType.XOR))
-			result += String.format("\tn%s[shape=diamond,label=\"%s\"];\n", g.getId().replace("-", ""), "XOR");
+			result += String.format("  n%s[shape=diamond,label=\"%s\"];\n", g.getId().replace("-", ""), "XOR");
 		for (Gateway g : process.getGateways(GatewayType.OR))
-			result += String.format("\tn%s[shape=diamond,label=\"%s\"];\n", g.getId().replace("-", ""), "OR");
+			result += String.format("  n%s[shape=diamond,label=\"%s\"];\n", g.getId().replace("-", ""), "OR");
 		for (Gateway g : process.getGateways(GatewayType.UNDEFINED))
-			result += String.format("\tn%s[shape=diamond,label=\"%s\"];\n", g.getId().replace("-", ""), "?");
-		
+			result += String.format("  n%s[shape=diamond,label=\"%s\"];\n", g.getId().replace("-", ""), "?");
+		result+="\n";
 		for (ControlFlow cf: process.getControlFlow()) {
-			result += String.format("\tn%s->n%s;\n", cf.getSource().getId().replace("-", ""), cf.getTarget().getId().replace("-", ""));
+			if (cf.getLabel()!=null || cf.getLabel()!="")
+				result += String.format("  n%s->n%s[label=\"%s\"];\n", cf.getSource().getId().replace("-", ""), cf.getTarget().getId().replace("-", ""), cf.getLabel());
+			else
+				result += String.format("  n%s->n%s;\n", cf.getSource().getId().replace("-", ""), cf.getTarget().getId().replace("-", ""));
 		}
 		result += "}";
 		
