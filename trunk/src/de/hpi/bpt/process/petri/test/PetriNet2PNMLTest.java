942e8f4d831aee1beb21ed629ba7fd2ad7e8bb00
christian.wiggert <christian.wiggert@1f0ec036-ed87-11dd-a45c-2d54f07c7ce3>
2011-05-20 16:46:51 +0000

    Added check for unstructered ORs in a process, addedserialization to PNML for PetriNet

---
+++ trunk/src/de/hpi/bpt/process/petri/test/PetriNet2PNMLTest.java
@@ -0,0 +1,63 @@
+package de.hpi.bpt.process.petri.test;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+
+import de.hpi.bpt.process.petri.PetriNet;
+import de.hpi.bpt.process.petri.PetriNet2PNML;
+import de.hpi.bpt.process.petri.Place;
+import de.hpi.bpt.process.petri.Transition;
+import junit.framework.TestCase;
+
+public class PetriNet2PNMLTest extends TestCase {
+
+	public void testSerialization() {
+		PetriNet net = new PetriNet();
+		Place p1 = new Place();
+		p1.setTokens(1);
+		Place p2 = new Place();
+		Place p3 = new Place();
+		Place p4 = new Place();
+		Place p5 = new Place();
+		Place p6 = new Place();
+		Transition t1 = new Transition();
+		Transition t2 = new Transition();
+		Transition t3 = new Transition();
+		Transition t4 = new Transition();
+		net.addFlow(p1, t1);
+		net.addFlow(t1, p2);
+		net.addFlow(t1, p3);
+		net.addFlow(p2, t2);
+		net.addFlow(p3, t3);
+		net.addFlow(t2, p4);
+		net.addFlow(t3, p5);
+		net.addFlow(p4, t4);
+		net.addFlow(p5, t4);
+		net.addFlow(t4, p6);
+		
+		Document doc = PetriNet2PNML.convert(net, PetriNet2PNML.LOLA);
+		assertNotNull(doc);
+		
+		DOMSource domSource = new DOMSource(doc);
+		
+		StreamResult streamResult = new StreamResult(System.out);
+		TransformerFactory tf = TransformerFactory.newInstance();
+		Transformer serializer;
+		try {
+			serializer = tf.newTransformer();
+			serializer.setOutputProperty(OutputKeys.INDENT,"yes");
+			serializer.transform(domSource, streamResult);
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		} 
+	}
+}
