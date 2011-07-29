5e23143a1882b830eaa07561bf7a5874a66e5857
matthias.weidlich <matthias.weidlich@1f0ec036-ed87-11dd-a45c-2d54f07c7ce3>
2011-07-29 17:02:30 +0000

    Update in cloning mechanism for graphs; made creators for (causal) behavioural profiles idempotent w.r.t. net system

--- src/de/hpi/bpt/process/petri/bp/construct/BPCreatorTree.java
+++ src/de/hpi/bpt/process/petri/bp/construct/BPCreatorTree.java
@@ -2,6 +2,8 @@ package de.hpi.bpt.process.petri.bp.construct;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hpi.bpt.process.petri.Node;
 import de.hpi.bpt.process.petri.PetriNet;
@@ -44,7 +46,28 @@ public class BPCreatorTree extends AbstractBPCreator implements BPCreator {
 	
 	public BehaviouralProfile deriveBehaviouralProfile(PetriNet pn, Collection<Node> nodes) {
 
-		WFTree wfTree = new WFTree(pn);
+		/*
+		 * The construction of the WF-tree may augment the original net. Therefore,
+		 * we clone the net and derive the WF-tree for the clone. We use a dedicated
+		 * clone method that provides us with an according node mapping between the 
+		 * original net and the clone.
+		 */
+		PetriNet netClone = null;
+		Map<Node, Node> nodeMapping = new HashMap<Node, Node>();
+		try {
+			netClone = (PetriNet) pn.clone(nodeMapping);
+		} catch (CloneNotSupportedException e) {
+			System.err.println("Clone not supported for PetriNet in BPCreatorTree. Take original net.");
+		}
+		// Fall back to original net
+		if (netClone == null) {
+			netClone = pn;
+			for (Node n : pn.getNodes())
+				nodeMapping.put(n, n);
+		}
+
+		
+		WFTree wfTree = new WFTree(netClone);
 		
 		BehaviouralProfile profile = new BehaviouralProfile(pn,nodes);
 		CharacteristicRelationType[][] matrix = profile.getMatrix();
@@ -60,14 +83,14 @@ public class BPCreatorTree extends AbstractBPCreator implements BPCreator {
 				if (index2 > index1)
 					continue;
 				
-				if (wfTree.areExclusive(t1, t2)) {
+				if (wfTree.areExclusive(nodeMapping.get(t1), nodeMapping.get(t2))) {
 					super.setMatrixEntry(matrix, index1, index2, CharacteristicRelationType.Exclusive);
 				}
-				else if (wfTree.areInterleaving(t1, t2)) {
+				else if (wfTree.areInterleaving(nodeMapping.get(t1), nodeMapping.get(t2))) {
 					super.setMatrixEntry(matrix, index1, index2, CharacteristicRelationType.InterleavingOrder);
 				}
-				else if (wfTree.areInOrder(t1, t2)) {
-					if (wfTree.areInStrictOrder(t1, t2))
+				else if (wfTree.areInOrder(nodeMapping.get(t1), nodeMapping.get(t2))) {
+					if (wfTree.areInStrictOrder(nodeMapping.get(t1), nodeMapping.get(t2)))
 						super.setMatrixEntryOrder(matrix, index1, index2);
 					else
 						super.setMatrixEntryOrder(matrix, index2, index1);
--- src/de/hpi/bpt/process/petri/bp/construct/CBPCreatorTree.java
+++ src/de/hpi/bpt/process/petri/bp/construct/CBPCreatorTree.java
@@ -2,6 +2,8 @@ package de.hpi.bpt.process.petri.bp.construct;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hpi.bpt.process.petri.Node;
 import de.hpi.bpt.process.petri.PetriNet;
@@ -31,7 +33,28 @@ public class CBPCreatorTree extends AbstractBPCreator implements CBPCreator {
 	
 	public CausalBehaviouralProfile deriveCausalBehaviouralProfile(PetriNet pn, Collection<Node> nodes) {
 
-		WFTree wfTree = new WFTree(pn);
+		/*
+		 * The construction of the WF-tree may augment the original net. Therefore,
+		 * we clone the net and derive the WF-tree for the clone. We use a dedicated
+		 * clone method that provides us with an according node mapping between the 
+		 * original net and the clone.
+		 */
+		PetriNet netClone = null;
+		Map<Node, Node> nodeMapping = new HashMap<Node, Node>();
+		try {
+			netClone = (PetriNet) pn.clone(nodeMapping);
+		} catch (CloneNotSupportedException e) {
+			System.err.println("Clone not supported for PetriNet in CBPCreatorTree. Take original net.");
+		}
+		// Fall back to original net
+		if (netClone == null) {
+			netClone = pn;
+			for (Node n : pn.getNodes())
+				nodeMapping.put(n, n);
+		}
+
+		
+		WFTree wfTree = new WFTree(netClone);
 		
 		CausalBehaviouralProfile profile = new CausalBehaviouralProfile(pn,nodes);
 		CharacteristicRelationType[][] matrix = profile.getMatrix();
@@ -42,7 +65,7 @@ public class CBPCreatorTree extends AbstractBPCreator implements CBPCreator {
 			for(Node t2 : profile.getNodes()) {
 				int index2 = profile.getNodes().indexOf(t2);
 				
-				if (wfTree.areCooccurring(t1, t2))
+				if (wfTree.areCooccurring(nodeMapping.get(t1), nodeMapping.get(t2)))
 					cooccurrenceMatrix[index1][index2] = true;
 				
 				/*
@@ -52,14 +75,14 @@ public class CBPCreatorTree extends AbstractBPCreator implements CBPCreator {
 				if (index2 > index1)
 					continue;
 				
-				if (wfTree.areExclusive(t1, t2)) {
+				if (wfTree.areExclusive(nodeMapping.get(t1), nodeMapping.get(t2))) {
 					super.setMatrixEntry(matrix, index1, index2, CharacteristicRelationType.Exclusive);
 				}
-				else if (wfTree.areInterleaving(t1, t2)) {
+				else if (wfTree.areInterleaving(nodeMapping.get(t1), nodeMapping.get(t2))) {
 					super.setMatrixEntry(matrix, index1, index2, CharacteristicRelationType.InterleavingOrder);
 				}
-				else if (wfTree.areInOrder(t1, t2)) {
-					if (wfTree.areInStrictOrder(t1, t2))
+				else if (wfTree.areInOrder(nodeMapping.get(t1), nodeMapping.get(t2))) {
+					if (wfTree.areInStrictOrder(nodeMapping.get(t1), nodeMapping.get(t2)))
 						super.setMatrixEntryOrder(matrix, index1, index2);
 					else
 						super.setMatrixEntryOrder(matrix, index2, index1);
@@ -74,14 +97,31 @@ public class CBPCreatorTree extends AbstractBPCreator implements CBPCreator {
 	public CausalBehaviouralProfile deriveCausalBehaviouralProfile(BehaviouralProfile bp) {
 		
 		PetriNet pn = bp.getNet();
+		
+		/*
+		 * The construction of the WF-tree may augment the original net. Therefore,
+		 * we clone the net and derive the WF-tree for the clone. We use a dedicated
+		 * clone method that provides us with an according node mapping between the 
+		 * original net and the clone.
+		 */
+		PetriNet netClone = null;
+		Map<Node, Node> nodeMapping = new HashMap<Node, Node>();
+		try {
+			netClone = (PetriNet) pn.clone(nodeMapping);
+		} catch (CloneNotSupportedException e) {
+			System.err.println("Clone not supported for PetriNet in BPCreatorTree. Take original net.");
+		}
+		// Fall back to original net
+		if (netClone == null)
+			netClone = pn;
+		
+		WFTree wfTree = new WFTree(netClone);
 
 		/*
 		 * Get the behavioural profile
 		 */
 		CausalBehaviouralProfile profile = new CausalBehaviouralProfile(pn, bp.getNodes());
 		profile.setMatrix(bp.getMatrix());	
-
-		WFTree wfTree = new WFTree(pn);
 
 		boolean[][] cooccurrenceMatrix = profile.getCooccurrenceMatrix();
 
@@ -90,7 +130,7 @@ public class CBPCreatorTree extends AbstractBPCreator implements CBPCreator {
 			for(Node t2 : profile.getNodes()) {
 				int index2 = profile.getNodes().indexOf(t2);
 				
-				if (wfTree.areCooccurring(t1, t2))
+				if (wfTree.areCooccurring(nodeMapping.get(t1), nodeMapping.get(t2)))
 					cooccurrenceMatrix[index1][index2] = true;
 			}
 		}
