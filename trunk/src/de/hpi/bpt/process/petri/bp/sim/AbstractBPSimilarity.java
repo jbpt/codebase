fd1053c8764b5a13c02f3c1795b28bb3b76e2ba3
matthias.weidlich <matthias.weidlich@1f0ec036-ed87-11dd-a45c-2d54f07c7ce3>
2011-09-22 06:12:10 +0000

    Update on BP similarities

--- trunk/src/de/hpi/bpt/process/petri/bp/sim/AbstractBPSimilarity.java
+++ trunk/src/de/hpi/bpt/process/petri/bp/sim/AbstractBPSimilarity.java
@@ -11,18 +11,52 @@ import de.hpi.bpt.process.petri.bp.BPAlignment;
 import de.hpi.bpt.process.petri.bp.BehaviouralProfile;
 import de.hpi.bpt.process.petri.bp.BehaviouralProfile.CharacteristicRelationType;
 
+/**
+ * Abstract class for BP similarities that contains utility functions
+ * for the concrete similarity implementations. Those mainly relate to 
+ * the computation of the size of a profile relation and the computation
+ * of the intersection of two profile relations.
+ * 
+ * Note that these computations are cached for behavioural profiles. Cache
+ * may be emptied by calling <code>invalidateCache()</code>.
+ * 
+ * @author matthias.weidlich
+ *
+ */
 public abstract class AbstractBPSimilarity implements BPSimilarity {
-
+	
+	/**
+	 * The cache for the size of a certain relation of a certain 
+	 * behavioural profile.
+	 */
 	private Map<BehaviouralProfile,Map<CharacteristicRelationType,Integer>> relationChache = new HashMap<BehaviouralProfile,Map<CharacteristicRelationType,Integer>>();
 	
+	/**
+	 * Standard method for the name of a similarity. Simply returns the class name.
+	 */
 	public String getName() {
 		return this.getClass().getName();
 	}
 	
+	/**
+	 * Computes the size of the intersection of a relation of two aligned behavioural profiles.
+	 * 
+	 * @param alignment, establishes the relation between two behavioural profiles
+	 * @param relation, the type of the relation for which the size of the intersection is determined
+	 * @return the size of the intersection of the relations of given type 
+	 */
 	protected int getSizeOfIntersectionOfRelation(BPAlignment alignment, CharacteristicRelationType relation) {
 		return getSizeOfIntersectionOfTwoRelations(alignment,relation,relation);
 	}
-	
+
+	/**
+	 * Computes the size of the intersection of two given relations of two aligned behavioural profiles.
+	 * 
+	 * @param alignment, establishes the relation between two behavioural profiles
+	 * @param relation1, the type of the considered relation in the first model
+	 * @param relation2, the type fo the considered relation in the second model
+	 * @return the size of the intersection of the two relations of given type 
+	 */
 	protected int getSizeOfIntersectionOfTwoRelations(BPAlignment alignment, CharacteristicRelationType relation1, CharacteristicRelationType relation2) {
 		int sizeOfIntersection = 0;
 		
@@ -42,11 +76,17 @@ public abstract class AbstractBPSimilarity implements BPSimilarity {
 					}
 				}
 			}
-		}
-				
+		}			
 		return sizeOfIntersection;
 	}
 	
+	/**
+	 * Computes the size of a given relation of the given behavioural profile.
+	 * 
+	 * @param profile, the behavioural profile 
+	 * @param relation, the type of the respective relation
+	 * @return the size of the relation in the given behavioural profile
+	 */
 	protected int getSizeOfRelation(BehaviouralProfile profile, CharacteristicRelationType relation) {
 		if (!relationChache.containsKey(profile))
 			relationChache.put(profile,new HashMap<BehaviouralProfile.CharacteristicRelationType, Integer>());
@@ -68,9 +108,10 @@ public abstract class AbstractBPSimilarity implements BPSimilarity {
 					sizeOfRelation++;
 			}
 		}
-		
+		// put into cache
 		relationChache.get(profile).put(relation,sizeOfRelation);
 		
+		// leverage symmetries to fill more details into the cache 
 		if (relation.equals(CharacteristicRelationType.StrictOrder))
 			relationChache.get(profile).put(CharacteristicRelationType.ReverseStrictOrder,sizeOfRelation);
 		if (relation.equals(CharacteristicRelationType.ReverseStrictOrder))
@@ -79,6 +120,11 @@ public abstract class AbstractBPSimilarity implements BPSimilarity {
 		return sizeOfRelation;
 	}
 	
-
+	/**
+	 * Resets the internal cache that stores the sizes of relations for behavioural profiles.
+	 */
+	public void invalidateCache() {
+		relationChache = new HashMap<BehaviouralProfile,Map<CharacteristicRelationType,Integer>>();		
+	}
 	
 }
