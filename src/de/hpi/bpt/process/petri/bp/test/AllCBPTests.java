c0c994d371540fd905c3c48249d7547a5b19f93f
matthias.weidlich <matthias.weidlich@1f0ec036-ed87-11dd-a45c-2d54f07c7ce3>
2011-09-22 06:12:10 +0000

    Update on BP similarities

---
+++ src/de/hpi/bpt/process/petri/bp/test/AllCBPTests.java
@@ -0,0 +1,19 @@
+package de.hpi.bpt.process.petri.bp.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllCBPTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllCBPTests.class.getName());
+		//$JUnit-BEGIN$
+		suite.addTestSuite(BPAlgebraTest.class);
+		suite.addTestSuite(CBPComputationTest.class);
+		suite.addTestSuite(CBPCreatorIdempotenceTest.class);
+		suite.addTestSuite(BPSimTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
