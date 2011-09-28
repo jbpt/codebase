751d47b092feedd7af5581a9b1b8cb75f5f34eb0
matthias.weidlich <matthias.weidlich@1f0ec036-ed87-11dd-a45c-2d54f07c7ce3>
2011-09-28 19:09:23 +0000

    some more comments in Alignment.java

--- trunk/src/de/hpi/bpt/alignment/Alignment.java
+++ trunk/src/de/hpi/bpt/alignment/Alignment.java
@@ -36,16 +36,41 @@ public class Alignment<E extends IEdge<V>,V extends IVertex>  {
 		 */
 		protected IGraph<E,V> graph2;
 
-		
+		/**
+		 * Correspondence relation to capture elementary and complex correspondences
+		 * between the first and the second graph.
+		 */
 		protected Map<V,Set<V>> correspondenceRelation = new HashMap<V, Set<V>>();
+		
 		/**
 		 * The reverse correspondence relation is maintained as well.
 		 */
 		protected Map<V,Set<V>> reversedCorrespondenceRelation = new HashMap<V, Set<V>>();
 
+		/**
+		 * Constructor that takes just the two graphs as input, for which
+		 * we want to define correspondences.
+		 * 
+		 * @param graph1, the first graph of the correspondences
+		 * @param graph2, the second graph of the correspondences
+		 */
 		public Alignment(IGraph<E,V> graph1, IGraph<E,V> graph2){
 			this.graph1 = graph1;
 			this.graph2 = graph2;
+		}
+		
+		/**
+		 * Constructor that copies the alignment given as input.
+		 * 
+		 * @param alignment, the alignment that shall be copied
+		 */
+		public Alignment(Alignment<E,V> alignment) {
+			this.graph1 = alignment.getFirstGraph();
+			this.graph2 = alignment.getSecondGraph();
+			
+			for (V v1 : alignment.getAlignedVerticesOfFirstGraph())
+				for (V v2 : alignment.getCorrespondingVerticesForVertexOfFirstGraph(v1))
+					this.addElementaryCorrespondence(v1, v2);
 		}
 		
 		public void addElementaryCorrespondence(V n1, V n2) {
@@ -73,6 +98,33 @@ public class Alignment<E extends IEdge<V>,V extends IVertex>  {
 				addComplexCorrespondence(n,n2);
 		}
 		
+		public void removeElementaryCorrespondence(V n1, V n2) {
+			if (this.correspondenceRelation.containsKey(n1))
+				this.correspondenceRelation.get(n1).remove(n2);
+			if (this.correspondenceRelation.get(n1).isEmpty())
+				this.correspondenceRelation.remove(n1);
+			
+			if (this.reversedCorrespondenceRelation.containsKey(n2))
+				this.reversedCorrespondenceRelation.get(n2).remove(n1);
+			if (this.reversedCorrespondenceRelation.get(n2).isEmpty())
+				this.reversedCorrespondenceRelation.remove(n2);
+		}
+		
+		public void removeComplexCorrespondence(Set<V> n1, V n2) {
+			for (V n : n1)
+				removeElementaryCorrespondence(n, n2);
+		}
+		
+		public void removeComplexCorrespondence(V n1, Set<V> n2) {
+			for (V n : n2)
+				removeElementaryCorrespondence(n1, n);
+		}
+		
+		public void removeComplexCorrespondence(Set<V> n1, Set<V> n2) {
+			for (V n : n1)
+				removeComplexCorrespondence(n,n2);
+		}
+		
 		public IGraph<E,V> getFirstGraph() {
 			return this.graph1;
 		}
@@ -82,19 +134,19 @@ public class Alignment<E extends IEdge<V>,V extends IVertex>  {
 		}
 
 		public Collection<V> getAlignedVerticesOfFirstGraph() {
-			return this.correspondenceRelation.keySet();
+			return new HashSet<V>(this.correspondenceRelation.keySet());
 		}
 
 		public Collection<V> getAlignedVerticesOfSecondGraph() {
-			return this.reversedCorrespondenceRelation.keySet();
+			return new HashSet<V>(this.reversedCorrespondenceRelation.keySet());
 		}
 
 		public Collection<V> getCorrespondingVerticesForVertexOfFirstGraph(V n) {
-			return this.correspondenceRelation.get(n);
+			return new HashSet<V>(this.correspondenceRelation.get(n));
 		}
 		
 		public Collection<V> getCorrespondingVerticesForVertexOfSecondGraph(V n) {
-			return this.reversedCorrespondenceRelation.get(n);
+			return new HashSet<V>(this.reversedCorrespondenceRelation.get(n));
 		}
 		
 		public boolean isOverlapping(){
@@ -141,12 +193,26 @@ public class Alignment<E extends IEdge<V>,V extends IVertex>  {
 			return true;
 		}
 		
+		/**
+		 * Normalise a label for comparison in the course of alignment initialisation:
+		 * <ul>
+		 * <li>set all characters in lower case</li>
+		 * <li>trim leading and trailing whitespaces</li>
+		 * </ul>
+		 * 
+		 * @param s, the label that shall be normalised
+		 * @return the normalised label
+		 */
 		protected static String normaliseLabel(String s) {
 			String result = s.toLowerCase();
 			result = result.trim();
 			return result;
 		}
 		
+		/**
+		 * Init the alignment with correspondences between nodes that
+		 * carry equal labels.
+		 */
 		public void initCorrespondenceRelation() {
 			for (V v1 : getFirstGraph().getVertices()) {
 				for (V v2 : getSecondGraph().getVertices()) {
