7e8e06f7a16a27d43723178af755a25a497ae253
tobiashoppe86 <tobiashoppe86@gmail.com>
2012-05-31 21:09:14 +0000

    Extended TransformationException with option to pass a detail message.

--- src/org/jbpt/utils/TransformationException.java
+++ src/org/jbpt/utils/TransformationException.java
@@ -2,6 +2,21 @@ package org.jbpt.utils;
 
 public class TransformationException extends Exception {
 
+	/**
+     * Constructs a new exception without detail message.
+     */
+	public TransformationException() {
+		super();
+	}
+
+	/**
+     * Constructs a new exception with the specified detail message.
+     * @param message the detail message.
+     */
+	public TransformationException(String message) {
+		super(message);
+	}
+	
 	private static final long serialVersionUID = 4274279008046583550L;
 
 }
