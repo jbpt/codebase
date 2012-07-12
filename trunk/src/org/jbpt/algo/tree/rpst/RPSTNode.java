8ad4c26fce76a11958a70a3ec6e55eddd36d6624
artem.polyvyanyy <artem.polyvyanyy@1f0ec036-ed87-11dd-a45c-2d54f07c7ce3>
2012-07-12 05:13:50 +0000

    New version of RPST. Enjoy! Still some testing to come.

--- trunk/src/org/jbpt/algo/tree/rpst/RPSTNode.java
+++ trunk/src/org/jbpt/algo/tree/rpst/RPSTNode.java
@@ -1,9 +1,16 @@
 package org.jbpt.algo.tree.rpst;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.jbpt.algo.tree.tctree.TCTreeNode;
+import org.jbpt.algo.tree.tctree.TCType;
+import org.jbpt.graph.DirectedEdge;
 import org.jbpt.graph.Fragment;
 import org.jbpt.graph.abs.IDirectedEdge;
 import org.jbpt.hypergraph.abs.IVertex;
+import org.jbpt.hypergraph.abs.Vertex;
 
 
 /**
@@ -14,16 +21,139 @@ import org.jbpt.hypergraph.abs.IVertex;
  * @param <E> Edge template.
  * @param <V> Vertex template.
  */
-public class RPSTNode<E extends IDirectedEdge<V>, V extends IVertex> extends TCTreeNode<E,V> {
-
-	protected boolean isQuasi = false;
-	
+public class RPSTNode<E extends IDirectedEdge<V>, V extends IVertex> extends Vertex {
+	// fragment entry
 	protected V entry = null;
-	
+	// fragment exit 
 	protected V exit = null;
-	
+	// fragment
 	protected Fragment<E,V> fragment = null;
+	// type of the triconnected component which induces this fragment
+	protected TCType type = TCType.UNDEFINED;
+	// link to the triconnected component which induces this fragment
+	private TCTreeNode<DirectedEdge,Vertex> tcnode = null;
+	// link to the RPST
+	private RPST<E,V> rpst = null;
+	
+	/**
+	 * Protected constructor.
+	 * 
+	 * @param rpst Link to the RPST this node belongs.
+	 * @param tcnode The triconnected component which induces this fragment.
+	 */
+	protected RPSTNode(RPST<E,V> rpst, TCTreeNode<DirectedEdge,Vertex> tcnode) {
+		this.rpst = rpst;
+		this.tcnode = tcnode;
+		this.type = tcnode.getType();
+		this.setName(tcnode.getName());
+	}
+	
+	/**
+	 * Get type of the triconnected component which induces this fragment.
+	 * 
+	 * @return Type of the triconnected component which induces this fragment. 
+	 */
+	public TCType getType() {
+		return this.type;
+	}
 
+	/**
+	 * Get entry of the fragment represented by this RPST node.
+	 * 
+	 * @return Entry of the fragment.
+	 */
+	public V getEntry() {
+		if (this.entry==null)
+			this.classifyBoundaries();
+		
+		return this.entry;
+	}
 	
+	/**
+	 * Get exit of the fragment represented by this RPST node.
+	 * 
+	 * @return Exit of the fragment.
+	 */
+	public V getExit() {
+		if (this.exit==null)
+			this.classifyBoundaries();
+		
+		return this.exit;
+	}
 	
+	/**
+	 * Get fragment represented by this RPST node.
+	 * 
+	 * @return Fragment.
+	 */
+	public Fragment<E,V> getFragment() {
+		if (this.fragment == null)
+			this.constructFragment();
+		
+		return this.fragment;
+	}
+
+	private void classifyBoundaries() {
+		Set<V> vertices = new HashSet<V>();
+		
+		for (E e : this.getFragment()) {
+			vertices.add(e.getSource());
+			vertices.add(e.getTarget());
+		}
+		
+		boolean flag = false;
+		V vv = null;
+		for (V v : vertices) {
+			if (this.rpst.diGraph.getIncomingEdges(v).isEmpty())
+				this.entry = v;
+			else if (this.rpst.diGraph.getOutgoingEdges(v).isEmpty())
+				this.exit = v;
+			else {
+				if (this.getFragment().containsAll(this.rpst.diGraph.getEdges(v))) continue;
+				
+				if (!flag) { flag = true; vv = v; }
+				else flag = false;
+				
+				if (this.getFragment().containsAll(this.rpst.diGraph.getOutgoingEdges(v)) ||
+						this.areDisjoint(this.getFragment(),this.rpst.diGraph.getIncomingEdges(v)))
+					this.entry = v;
+				
+				if (this.getFragment().containsAll(this.rpst.diGraph.getIncomingEdges(v)) ||
+						this.areDisjoint(this.getFragment(),this.rpst.diGraph.getOutgoingEdges(v)))
+					this.exit = v;
+			}
+		}
+		
+		if (flag) {
+			this.entry = vv;
+			this.exit = vv;
+		}
+	}
+	
+	private boolean areDisjoint(Collection<E> c1, Collection<E> c2) {
+		for (E e : c2) 
+			if (c1.contains(e))
+				return false;
+		
+		return true;
+	}
+
+	private void constructFragment() {
+		this.fragment = new Fragment<E,V>(this.rpst.diGraph);
+		
+		for (RPSTNode<E,V> node : this.rpst.getChildren(this)) {
+			this.fragment.addAll(node.getFragment());
+		}
+		
+		for (DirectedEdge edge : this.tcnode.getSkeleton().getOriginalEdges()) {
+			if (this.rpst.extraEdges.contains(edge)) continue;
+			
+			this.fragment.add(this.rpst.ne2oe.get(edge));
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s: (%s,%s) - %s", this.getName(), this.getEntry(), this.getExit(), this.getFragment());
+	}
 }
--- trunk/src/org/jbpt/test/tree/RPSTTest.java
+++ trunk/src/org/jbpt/test/tree/RPSTTest.java
@@ -1,22 +1,206 @@
 package org.jbpt.test.tree;
 
-import org.jbpt.algo.tree.rpst.RPST;
-import org.jbpt.algo.tree.rpst.RPSTNode;
-import org.jbpt.algo.tree.tctree.TCType;
-import org.jbpt.graph.DirectedEdge;
-import org.jbpt.graph.DirectedGraph;
-import org.jbpt.graph.abs.IDirectedEdge;
-import org.jbpt.hypergraph.abs.Vertex;
-import org.jbpt.pm.Activity;
-import org.jbpt.pm.ControlFlow;
-import org.jbpt.pm.FlowNode;
-import org.jbpt.pm.Gateway;
-import org.jbpt.pm.ProcessModel;
-import org.jbpt.pm.XorGateway;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import junit.framework.TestCase;
 
+import org.jbpt.algo.tree.rpst.RPST;
+import org.jbpt.algo.tree.rpst.RPSTNode;
+import org.jbpt.graph.DirectedEdge;
+import org.jbpt.graph.MultiDirectedGraph;
+import org.jbpt.hypergraph.abs.Vertex;
+import org.jbpt.utils.IOUtils;
+
 public class RPSTTest extends TestCase {
+	
+	/**
+	 * This test is taken from: 
+	 * Artem Polyvyanyy, Jussi Vanhatalo, Hagen Völzer: Simplified Computation and Generalization of the Refined Process Structure Tree. WS-FM 2010: 25-41. 
+	 * See Figures 8 & 9.
+	 */
+	/*public void testWSFM10_Figures8and9() {
+		MultiDirectedGraph g = new MultiDirectedGraph();
+		
+		Vertex s = new Vertex("s");
+		Vertex y = new Vertex("y");
+		Vertex z = new Vertex("z");
+		Vertex t = new Vertex("t");
+		
+		g.addEdge(s,y);
+		g.addEdge(y,z);
+		g.addEdge(y,z);
+		g.addEdge(z,y);
+		g.addEdge(z,t);
+		
+		RPST<DirectedEdge,Vertex> rpst = new RPST<DirectedEdge,Vertex>(g);
+		IOUtils.toFile("rpst.dot", rpst.toDOT());
+		rpst.test();
+		
+		performBasicChecks(g,rpst);
+		
+		for (RPSTNode<DirectedEdge,Vertex> node : rpst.getRPSTNodes()) {
+			System.out.println(node);
+		}
+	}*/
+	
+	/**
+	 * This test is taken from: 
+	 * Artem Polyvyanyy, Jussi Vanhatalo, Hagen Völzer: Simplified Computation and Generalization of the Refined Process Structure Tree. WS-FM 2010: 25-41. 
+	 * See Figure 6.
+	 */
+	/*public void testWSFM10_Figure6() {
+		MultiDirectedGraph g = new MultiDirectedGraph();
+		
+		Vertex s = new Vertex("s");
+		Vertex u = new Vertex("u");
+		Vertex v = new Vertex("v");
+		Vertex w = new Vertex("w");
+		Vertex x = new Vertex("x");
+		Vertex t = new Vertex("t");
+		
+		g.addEdge(s,u);
+		g.addEdge(u,v);
+		g.addEdge(v,x);
+		g.addEdge(u,w);
+		g.addEdge(w,x);
+		g.addEdge(v,w);
+		g.addEdge(x,t);
+		
+		RPST<DirectedEdge,Vertex> rpst = new RPST<DirectedEdge,Vertex>(g);
+		IOUtils.toFile("rpst.dot", rpst.toDOT());
+		rpst.test();
+		
+		performBasicChecks(g,rpst);
+		
+		for (RPSTNode<DirectedEdge,Vertex> node : rpst.getRPSTNodes()) {
+			System.out.println(node);
+		}
+	}*/
+	
+	/**
+	 * This test is taken from: 
+	 * Jussi Vanhatalo, Hagen Völzer, Jana Koehler: The Refined Process Structure Tree. BPM 2008: 100-115.  
+	 * See Figure 11.
+	 */
+	/*public void testBPM08_Figure6() {
+		MultiDirectedGraph g = new MultiDirectedGraph();
+		
+		Vertex s = new Vertex("s");
+		Vertex t = new Vertex("t");
+		Vertex v1 = new Vertex("v1");
+		Vertex v2 = new Vertex("v2");
+		Vertex v3 = new Vertex("v3");
+		Vertex v4 = new Vertex("v4");
+		Vertex v5 = new Vertex("v5");
+		Vertex v6 = new Vertex("v6");
+		Vertex v7 = new Vertex("v7");
+		
+		g.addVertex(s);
+		g.addVertex(t);
+		g.addVertex(v1);
+		g.addVertex(v2);
+		g.addVertex(v3);
+		g.addVertex(v4);
+		g.addVertex(v5);
+		g.addVertex(v6);
+		g.addVertex(v7);
+		
+		g.addEdge(s,v1);
+		g.addEdge(s,v2);
+		g.addEdge(v1,v3);
+		g.addEdge(v1,v5);
+		g.addEdge(v2,v5);
+		g.addEdge(v3,v2);
+		g.addEdge(v3,v4);
+		g.addEdge(v4,v1);
+		g.addEdge(v4,v2);
+		g.addEdge(v5,v6);
+		g.addEdge(v5,v7);
+		g.addEdge(v6,v5);
+		g.addEdge(v6,v7);
+		g.addEdge(v7,v5);
+		g.addEdge(v7,t);
+		
+		RPST<DirectedEdge,Vertex> rpst = new RPST<DirectedEdge,Vertex>(g);
+		IOUtils.toFile("rpst.dot", rpst.toDOT());
+		rpst.test();
+		
+		performBasicChecks(g,rpst);
+		
+		for (RPSTNode<DirectedEdge,Vertex> node : rpst.getRPSTNodes()) {
+			System.out.println(node);
+		}
+	}*/
+	
+	/**
+	 * This test is taken from: 
+	 * Artem Polyvyanyy, Jussi Vanhatalo, Hagen Völzer: Simplified Computation and Generalization of the Refined Process Structure Tree. WS-FM 2010: 25-41. 
+	 * See Figure 11.
+	 */
+	/*public void testWSFM10_Figure11() {
+		MultiDirectedGraph g = new MultiDirectedGraph();
+		
+		Vertex s = new Vertex("s");
+		Vertex t = new Vertex("t");
+		Vertex u = new Vertex("u");
+		Vertex v = new Vertex("v");
+		Vertex a1 = new Vertex("a1");
+		Vertex a2 = new Vertex("a2");
+		
+		g.addEdge(s,u);
+		g.addEdge(u,a1);
+		g.addEdge(a1,u);
+		g.addEdge(u,a2);
+		g.addEdge(a2,v);
+		g.addEdge(v,u);
+		g.addEdge(v,t);
+		
+		RPST<DirectedEdge,Vertex> rpst = new RPST<DirectedEdge,Vertex>(g);
+		IOUtils.toFile("rpst.dot", rpst.toDOT());
+		rpst.test();
+		
+		performBasicChecks(g,rpst);
+		
+		for (RPSTNode<DirectedEdge,Vertex> node : rpst.getRPSTNodes()) {
+			System.out.println(node);
+		}
+	}*/
+	
+	/**
+	 * This test is taken from: 
+	 * Artem Polyvyanyy, Jussi Vanhatalo, Hagen Völzer: Simplified Computation and Generalization of the Refined Process Structure Tree. WS-FM 2010: 25-41. 
+	 * See Figure 12(a).
+	 * TODO finish test
+	 */
+	public void testWSFM10_Figure12a() {
+		MultiDirectedGraph g = new MultiDirectedGraph();
+		
+		Vertex s = new Vertex("s");
+		Vertex t = new Vertex("t");
+		Vertex u = new Vertex("u");
+		Vertex v = new Vertex("v");
+		Vertex a1 = new Vertex("a1");
+		Vertex a2 = new Vertex("a2");
+		
+		g.addEdge(s,u);
+		g.addEdge(u,a1);
+		g.addEdge(a1,u);
+		g.addEdge(u,a2);
+		g.addEdge(a2,v);
+		g.addEdge(v,u);
+		g.addEdge(v,t);
+		
+		RPST<DirectedEdge,Vertex> rpst = new RPST<DirectedEdge,Vertex>(g);
+		IOUtils.toFile("rpst.dot", rpst.toDOT());
+		rpst.test();
+		
+		performBasicChecks(g,rpst);
+		
+		for (RPSTNode<DirectedEdge,Vertex> node : rpst.getRPSTNodes()) {
+			System.out.println(node);
+		}
+	}
 	
 	/*public void testTrivialGraph2() {
 		System.out.println("========================================================");
@@ -61,61 +245,24 @@ public class RPSTTest extends TestCase {
 		for (RPSTNode<DirectedEdge,Vertex> node : rpst.getVertices()) {
 			System.out.println(node.getName() + " : " + node.getFragment());
 		}
-	}
+	}*/
 	
-	public void testBPM08Fig11() {
-		System.out.println("========================================================");
-		System.out.println("BPM08 Fig.11");
-		System.out.println("========================================================");
-		
-		DirectedGraph g = new DirectedGraph();
-		
-		Vertex s = new Vertex("s");
-		Vertex t = new Vertex("t");
-		Vertex v1 = new Vertex("v1");
-		Vertex v2 = new Vertex("v2");
-		Vertex v3 = new Vertex("v3");
-		Vertex v4 = new Vertex("v4");
-		Vertex v5 = new Vertex("v5");
-		Vertex v6 = new Vertex("v6");
-		Vertex v7 = new Vertex("v7");
-		
-		g.addVertex(s);
-		g.addVertex(t);
-		g.addVertex(v1);
-		g.addVertex(v2);
-		g.addVertex(v3);
-		g.addVertex(v4);
-		g.addVertex(v5);
-		g.addVertex(v6);
-		g.addVertex(v7);
-		
-		g.addEdge(s,v1);
-		g.addEdge(s,v2);
-		g.addEdge(v1,v3);
-		g.addEdge(v1,v5);
-		g.addEdge(v2,v5);
-		g.addEdge(v3,v2);
-		g.addEdge(v3,v4);
-		g.addEdge(v4,v1);
-		g.addEdge(v4,v2);
-		g.addEdge(v5,v6);
-		g.addEdge(v5,v7);
-		g.addEdge(v6,v5);
-		g.addEdge(v6,v7);
-		g.addEdge(v7,v5);
-		g.addEdge(v7,t);
-		
-		RPST<DirectedEdge,Vertex> rpst = new RPST<DirectedEdge,Vertex>(g);
-		
-		System.out.println(rpst);
-		
-		for (RPSTNode<DirectedEdge,Vertex> node : rpst.getVertices()) {
-			System.out.println(node.getName() + " : " + node.getFragment());
+	private void performBasicChecks(MultiDirectedGraph g, RPST<DirectedEdge, Vertex> rpst) {
+		for (RPSTNode<DirectedEdge,Vertex> node : rpst.getRPSTNodes()) {
+			assertTrue(g.getEdges().containsAll(node.getFragment()));
+			
+			Collection<DirectedEdge> edges = new ArrayList<DirectedEdge>();
+			for (RPSTNode<DirectedEdge,Vertex> child : rpst.getChildren(node)) {
+				edges.addAll(child.getFragment());
+			}
+			
+			assertTrue(node.getFragment().containsAll(edges));
 		}
 	}
 	
-	public void testSimpleGraph() {
+	
+	
+	/*public void testSimpleGraph() {
 		System.out.println("========================================================");
 		System.out.println("Simple Graph");
 		System.out.println("========================================================");
