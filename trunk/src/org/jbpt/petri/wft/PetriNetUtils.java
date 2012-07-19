55b55c5b44a183186db1740b909f8d61fd41880a
matthias.weidlich <matthias.weidlich@1f0ec036-ed87-11dd-a45c-2d54f07c7ce3>
2012-07-19 06:22:27 +0000

    some fixes in WFTree

--- trunk/src/org/jbpt/petri/wft/PetriNetUtils.java
+++ trunk/src/org/jbpt/petri/wft/PetriNetUtils.java
@@ -81,7 +81,7 @@ public class PetriNetUtils {
 	public static void relinkIncomingArcs(PetriNet net, Node from, Node to) {
 		for (Flow f : net.getFlow()) {
 			if (f.getTarget().equals(from)) {
-				net.addFlow(f.getSource(), to);
+				net.addFreshFlow(f.getSource(), to);
 				net.removeEdge(f);
 			}
 		}
@@ -90,7 +90,7 @@ public class PetriNetUtils {
 	public static void relinkOutgoingArcs(PetriNet net, Node from, Node to) {
 		for (Flow f : net.getFlow()) {
 			if (f.getSource().equals(from)) {
-				net.addFlow(to, f.getTarget());
+				net.addFreshFlow(to, f.getTarget());
 				net.removeEdge(f);
 			}
 		}
