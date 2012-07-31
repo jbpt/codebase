9bf2f3d4ead368525a5b7d475b2502014fcdf30a
felix.mannhardt <felix.mannhardt@gmail.com>
2012-07-31 05:31:52 +0000

    Converted jBPT to a Maven Project: Building requires installed Maven 2 and is done using "mvn clean install" in the base directory. All generated files (test output,jar) will be in directory "target". Main sourcecode belongs in "src/main/java". Test sourcecode belongs in "src/test/java". Added OSGi bundle MANIFEST headers

---
+++ src/test/java/org/jbpt/test/petri/unfolding/SoundUnfoldingExtensiveTestA.java
@@ -0,0 +1,58 @@
+package org.jbpt.test.petri.unfolding;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.jbpt.petri.NetSystem;
+import org.jbpt.petri.Place;
+import org.jbpt.petri.Transition;
+import org.jbpt.petri.unfolding.SoundUnfolding;
+import org.jbpt.pm.ProcessModel;
+import org.jbpt.pm.io.JSON2Process;
+import org.jbpt.pm.structure.ProcessModel2NetSystem;
+import org.jbpt.throwable.SerializationException;
+import org.jbpt.throwable.TransformationException;
+
+public class SoundUnfoldingExtensiveTestA extends TestCase {
+
+	protected static final String MODELS_DIR = "src/test/resources/models/process_json/acyclic/original";
+	
+	public void testComparison() throws SerializationException, IOException, TransformationException {
+		File modelsDir = new File(MODELS_DIR);
+		
+		int count = 0;
+		for (String name : modelsDir.list()) {
+			if (name.endsWith(".json")) {
+				ProcessModel p = loadProcess(MODELS_DIR + File.separator + name);
+				NetSystem net = ProcessModel2NetSystem.transform(p);
+				int cp = 1; int ct = 1;
+				for (Place place : net.getPlaces()) place.setName("p"+cp++);
+				for (Transition trans : net.getTransitions()) trans.setName("t"+ct++);
+				net.loadNaturalMarking();
+				
+				SoundUnfolding unf = new SoundUnfolding(net);
+				System.out.println(count + " - " + name);
+				
+				assertEquals(0,unf.getLocallyUnsafeConditions().size());
+				
+				if (++count==200) break;
+			}
+		}
+	}
+	
+	protected ProcessModel loadProcess(String filename) throws SerializationException, IOException {
+		String line;
+		StringBuilder sb = new StringBuilder();
+		BufferedReader reader = new BufferedReader(new FileReader(filename));
+		while ((line = reader.readLine()) != null) {
+			sb.append(line);
+		}
+		reader.close();
+		return JSON2Process.convert(sb.toString());
+	}
+		
+}
