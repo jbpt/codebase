5344058721ce35a71e1fbcbe734bbf5fc5f1596d
matthias.weidlich <matthias.weidlich@1f0ec036-ed87-11dd-a45c-2d54f07c7ce3>
2012-10-06 19:40:22 +0000

    added xml loading to io utils

--- trunk/src/main/java/org/jbpt/utils/IOUtils.java
+++ trunk/src/main/java/org/jbpt/utils/IOUtils.java
@@ -3,12 +3,26 @@ package org.jbpt.utils;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Set;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 public class IOUtils {
 
@@ -160,5 +174,20 @@ public class IOUtils {
 			e.printStackTrace();
 		}
 	}
+	
+    public static Document loadDocumentFromFile(String fileName) throws ParserConfigurationException, SAXException, IOException {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document document = builder.parse(new File(fileName));
 
+		return document;
+	}
+	
+	public static void saveDocumentToFile(Document doc, String fileName) throws TransformerFactoryConfigurationError, FileNotFoundException, TransformerException {
+		Transformer transformer = TransformerFactory.newInstance().newTransformer();
+		DOMSource        source = new DOMSource( doc );
+		FileOutputStream os     = new FileOutputStream( new File(fileName) );
+		StreamResult     result = new StreamResult( os );
+		transformer.transform( source, result );
+	}
 }
