9fccf6b9e73a930f5e04a4cca23927e8469d9061
artem.polyvyanyy <artem.polyvyanyy@1f0ec036-ed87-11dd-a45c-2d54f07c7ce3>
2012-12-13 07:43:45 +0000

    jbpt-petri: Minor changes to step of the net system.

--- jbpt-petri/src/main/java/org/jbpt/petri/IStep.java
+++ jbpt-petri/src/main/java/org/jbpt/petri/IStep.java
@@ -1,12 +1,14 @@
 package org.jbpt.petri;
 
+import org.jbpt.hypergraph.abs.IVertex;
+
 /**
  * Interface to a step in a net system.
  * 
  * @author Artem Polyvyanyy
  */
 public interface IStep<F extends IFlow<N>, N extends INode, P extends IPlace, T extends ITransition, M extends IMarking<F,N,P,T>>
-	extends ICommand<F,N,P,T,M> {
+	extends ICommand<F,N,P,T,M>, IVertex {
 	
 	public T getTransition();
 }
