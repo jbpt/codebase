280383af05ab0d6131d3ef455c1f406e3b7de8ad
artem.polyvyanyy <artem.polyvyanyy@1f0ec036-ed87-11dd-a45c-2d54f07c7ce3>
2013-05-05 13:10:52 +0000

    jbpt-petri: Progress with untangling optimization.

--- trunk/jbpt-petri/src/main/java/org/jbpt/petri/untangling/AbstractBaselineRepresentativeUntangling.java
+++ trunk/jbpt-petri/src/main/java/org/jbpt/petri/untangling/AbstractBaselineRepresentativeUntangling.java
@@ -1,5 +1,6 @@
 package org.jbpt.petri.untangling;
 
+import java.util.HashSet;
 import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -10,6 +11,7 @@ import org.jbpt.petri.IMarking;
 import org.jbpt.petri.INetSystem;
 import org.jbpt.petri.INode;
 import org.jbpt.petri.IPlace;
+import org.jbpt.petri.IRun;
 import org.jbpt.petri.ITransition;
 import org.jbpt.petri.structure.PetriNetStructuralChecks;
 import org.jbpt.petri.unfolding.IBPNode;
@@ -40,6 +42,84 @@ public class AbstractBaselineRepresentativeUntangling<BPN extends IBPNode<N>, C 
 	
 	@Override
 	protected void constructRuns(INetSystem<F,N,P,T,M> system) {
+		switch (this.setup.SIGNIFICANCE_CHECK) {
+			case EXHAUSTIVE:
+				this.constructRunsExhaustive(system);
+				break;
+			case HASHMAP_BASED:
+				this.constructRunsHashmapBased(system);
+				break;
+			case TREE_OF_RUNS:
+				this.constructRunsTreeOfRuns(system);
+				break;
+			default:
+				this.constructRunsExhaustive(system);
+		}
+	}
+	
+	private void constructRunsExhaustive(INetSystem<F,N,P,T,M> system) {
+		Queue<IRun<F,N,P,T,M>> queue = new ConcurrentLinkedQueue<>();
+		IRun<F,N,P,T,M> ini = this.createRun(system);
+		
+		// if system has no conflicts and is acyclic its untangling is trivial
+		PetriNetStructuralChecks<F,N,P,T> sc = new PetriNetStructuralChecks<>();
+		DirectedGraphAlgorithms<F,N> dga = new DirectedGraphAlgorithms<>();
+		if (sc.isConflictFree(system) && dga.isAcyclic(system)) {
+			Set<T> PE = ini.getPossibleExtensions();
+			while (!PE.isEmpty()) {
+				ini.append(PE.iterator().next());
+			}
+			this.significantRunCounter++;
+			this.runs.add(ini);
+			return;
+		}
+		
+		// perform complex stuff
+		queue.add(ini);
+		this.significantRunCounter++;
+		
+		while (!queue.isEmpty()) {
+			IRun<F,N,P,T,M> run = queue.poll();
+			
+			// safeness check (extra)
+			if (run.size()>0 && !run.get(run.size()-1).getOutputMarking().isSafe()) {
+				this.safe = false;
+				return;
+			}
+			
+			// get possible extensions of the run
+			Set<T> PE = run.getPossibleExtensions();
+			
+			if (PE.isEmpty()) {
+				if (setup.REDUCE) this.prune(run);
+				this.runs.add(run);
+			}
+			else {
+				boolean allExtensionsInsignificant = true;
+				for (T t : PE) {
+					IRun<F,N,P,T,M> freshRun = run.clone();
+					freshRun.append(t);
+					
+					if (this.isSignificant(freshRun)) {
+						queue.add(freshRun);
+						this.significantRunCounter++;
+						allExtensionsInsignificant = false;
+					}
+				}
+				
+				if (allExtensionsInsignificant) {
+					if (setup.REDUCE) this.prune(run);
+					this.runs.add(run);
+				}
+			}
+		}
+		
+		if (setup.REDUCE) {
+			this.reduceSubruns();
+		}
+	}
+	
+	private void constructRunsHashmapBased(INetSystem<F,N,P,T,M> system) {
 		Queue<IUntanglingRun<F,N,P,T,M>> queue = new ConcurrentLinkedQueue<>();
 		IUntanglingRun<F,N,P,T,M> ini = this.createUntanglingRun(system);
 		
@@ -98,6 +178,69 @@ public class AbstractBaselineRepresentativeUntangling<BPN extends IBPNode<N>, C 
 		
 		if (setup.REDUCE) {
 			this.reduceSubruns();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void constructRunsTreeOfRuns(INetSystem<F,N,P,T,M> system) {
+		this.torRoot = new TreeStep<F,N,P,T,M>(system,null,null,null,(M)system.getMarking().clone(),0);
+		this.torRoot.index = new TreeStepIndex<>(); 
+		this.torLeaves = new HashSet<>();
+		
+		Queue<TreeStep<F,N,P,T,M>> queue = new ConcurrentLinkedQueue<>();		
+		
+		// if system has no conflicts and is acyclic its untangling is trivial
+		PetriNetStructuralChecks<F,N,P,T> sc = new PetriNetStructuralChecks<>();
+		DirectedGraphAlgorithms<F,N> dga = new DirectedGraphAlgorithms<>();
+		if (sc.isConflictFree(system) && dga.isAcyclic(system)) {
+			IRun<F,N,P,T,M> ini = this.createRun(system);
+			Set<T> PE = ini.getPossibleExtensions();
+			while (!PE.isEmpty()) {
+				ini.append(PE.iterator().next());
+			}
+			this.significantRunCounter++;
+			this.runs.add(ini);
+			return;
+		}
+		
+		// perform complex stuff
+		if (!this.torRoot.isSafe()) {
+			this.safe = false;
+			return;
+		}
+		
+		queue.add(this.torRoot);
+		this.significantRunCounter++;
+		
+		while (!queue.isEmpty()) {
+			TreeStep<F,N,P,T,M> curr = queue.poll();
+			Set<TreeStep<F,N,P,T,M>> PE = curr.getPossibleExtensions();
+			
+			if (PE.isEmpty()) {
+				if (curr.transition!=null)
+					this.torLeaves.add(curr);
+			}
+			else {
+				boolean allExtensionsInsignificant = true;
+				
+				for (TreeStep<F,N,P,T,M> ext : PE) {
+					if (ext.isSignificant()) {
+						if (!ext.isSafe()) {
+							this.safe = false;
+							return;
+						}
+						
+						allExtensionsInsignificant = false;
+						
+						queue.add(ext);
+						this.significantRunCounter++;
+					}
+				}
+				
+				if (allExtensionsInsignificant) {
+					this.torLeaves.add(curr);
+				}
+			}
 		}
 	}
 }
---
+++ trunk/jbpt-petri/src/main/java/org/jbpt/petri/untangling/TreeStepIndex.java
@@ -0,0 +1,88 @@
+package org.jbpt.petri.untangling;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.jbpt.petri.IFlow;
+import org.jbpt.petri.IMarking;
+import org.jbpt.petri.INode;
+import org.jbpt.petri.IPlace;
+import org.jbpt.petri.ITransition;
+
+public class TreeStepIndex<F extends IFlow<N>, N extends INode, P extends IPlace, T extends ITransition, M extends IMarking<F,N,P,T>> 
+{
+	HashMap<TreeStep<F,N,P,T,M>,Integer> s2p = new HashMap<>();
+	HashMap<Interval,Set<TreeStep<F,N,P,T,M>>> i2s = new HashMap<>();
+	
+	boolean isSignificant = true;
+
+	class Interval {
+		private int l;
+	    private int r;
+	    
+	    public Interval(int l, int r){
+	        this.l = l;
+	        this.r = r;
+	    }
+	    
+	    public int getL() { return l; }
+	    public int getR() { return r; }
+	    public void setL(int l) { this.l = l; }
+	    public void setR(int r) { this.r = r; }
+	    
+	    @Override
+		public String toString() {
+			return "["+this.l+","+this.r+"]";
+		}
+	}
+	
+	public TreeStepIndex() {}
+	
+	public void process(TreeStep<F,N,P,T,M> step) {		
+		Integer preLast = this.s2p.get(step);
+		this.s2p.put(step,step.getPosition());
+		
+		if (preLast!=null) {
+			Interval interval = new Interval(preLast,step.getPosition());
+			
+			Set<TreeStep<F,N,P,T,M>> steps = new HashSet<>();
+			
+			TreeStep<F,N,P,T,M> s = step;
+			while (s.getTransition()!=null) {
+				if (s.getPosition()>interval.getL() && s.getPosition()<interval.getR())
+					steps.add(s);
+				
+				if (s.getPosition()<interval.getL() || s.getPosition()>interval.getR())
+					steps.remove(s);
+					
+				s = s.getParent();
+			}
+			
+			this.i2s.put(interval,steps);
+		}
+		
+		for (Map.Entry<Interval,Set<TreeStep<F,N,P,T,M>>> entry : this.i2s.entrySet()) {
+			entry.getValue().remove(step);
+			
+			if (entry.getValue().isEmpty())
+				this.isSignificant = false;
+		}
+	}
+
+	public boolean isSignificant() {
+		return this.isSignificant;
+	}
+
+	@Override
+	protected TreeStepIndex<F,N,P,T,M> clone() {
+		TreeStepIndex<F,N,P,T,M> clone = new TreeStepIndex<>();
+		
+		clone.isSignificant = this.isSignificant;
+		clone.i2s = new HashMap<>(this.i2s);
+		clone.s2p = new HashMap<>(this.s2p);
+		
+		return clone;
+	}
+}
