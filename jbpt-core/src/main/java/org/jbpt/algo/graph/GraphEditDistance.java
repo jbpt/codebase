f67e6fb81a686f952cbab0da9a6d77125370b9dd
matthias.weidlich <matthias.weidlich@1f0ec036-ed87-11dd-a45c-2d54f07c7ce3>
2013-11-05 17:10:37 +0000

    bug fix in GED computation

--- jbpt-core/src/main/java/org/jbpt/algo/graph/GraphEditDistance.java
+++ jbpt-core/src/main/java/org/jbpt/algo/graph/GraphEditDistance.java
@@ -33,12 +33,6 @@ public class GraphEditDistance<G extends IDirectedGraph<E,V>,E extends IDirected
 	
 	private int distance = -1; 
 	
-	public class VPair {
-		public V v1;
-		public V v2;
-		public VPair(V v1, V v2) { this.v1 = v1; this.v2 = v2; }
-	}
-	
 	public GraphEditDistance(G g1, G g2) {
 		this.g1 = g1;
 		this.g2 = g2;
@@ -88,43 +82,53 @@ public class GraphEditDistance<G extends IDirectedGraph<E,V>,E extends IDirected
 		
 		int skippedVertices = totalNrVertices - verticesFrom1Used.size() - verticesFrom2Used.size();
 
-		Set<VPair> edgesIn1 = new HashSet<VPair>();
-		for (E e : g1.getEdges())
-			edgesIn1.add(new VPair(e.getSource(), e.getTarget()));
-		Set<VPair> edgesIn2 = new HashSet<VPair>();
-		for (E e : g2.getEdges())
-			edgesIn2.add(new VPair(e.getSource(), e.getTarget()));
+		Set<E> groupedIn1 = new HashSet<E>();
+		Set<E> groupedIn2 = new HashSet<E>();
+
+		Set<E> mappedIn1 = new HashSet<E>();
+		Set<E> mappedIn2 = new HashSet<E>();
 		
-		Collection<VPair> translatedEdgesIn1 = new HashSet<VPair>();
-		Collection<VPair> translatedEdgesIn2 = new HashSet<VPair>();
-
-		int groupedEdges = 0;
-		for (VPair e1: edgesIn1){
-			Integer gsrc = vid1togid.get(e1.v1);
-			Integer gtgt = vid1togid.get(e1.v2);
+		for (E e1 : g1.getEdges()) {
+			Integer gsrc = vid1togid.get(e1.getSource());
+			Integer gtgt = vid1togid.get(e1.getTarget());
 			if ((gsrc != null) && (gtgt != null)) {
 				if (gsrc.equals(gtgt))
-					groupedEdges += 1;
-				else
-					translatedEdgesIn1.add(new VPair(e1.v1,e1.v2));
-				
+					groupedIn1.add(e1);
+				else {
+					for (E e2 : g2.getEdges()) {
+						Collection<V> alignedToE1Source = this.alignment.getCorrespondingEntitiesForEntityOfFirstModel(e1.getSource());
+						Collection<V> alignedToE1Target = this.alignment.getCorrespondingEntitiesForEntityOfFirstModel(e1.getTarget());
+						
+						if (alignedToE1Source.contains(e2.getSource()) && alignedToE1Target.contains(e2.getTarget())) {
+							mappedIn1.add(e1);
+						}
+					}
+				}
 			}
 		}
-		for (VPair e2: edgesIn2){
-			Integer gsrc = vid2togid.get(e2.v1);
-			Integer gtgt = vid2togid.get(e2.v2);
+		
+		for (E e2 : g2.getEdges()) {
+			Integer gsrc = vid2togid.get(e2.getSource());
+			Integer gtgt = vid2togid.get(e2.getTarget());
 			if ((gsrc != null) && (gtgt != null)) {
 				if (gsrc.equals(gtgt))
-					groupedEdges += 1;
-				else
-					translatedEdgesIn2.add(new VPair(e2.v1,e2.v2));
-				
+					groupedIn2.add(e2);
+				else {
+					for (E e1 : g1.getEdges()) {
+						Collection<V> alignedToE2Source = this.alignment.getCorrespondingEntitiesForEntityOfSecondModel(e2.getSource());
+						Collection<V> alignedToE2Target = this.alignment.getCorrespondingEntitiesForEntityOfSecondModel(e2.getTarget());
+						
+						if (alignedToE2Source.contains(e1.getSource()) && alignedToE2Target.contains(e1.getTarget())) {
+							mappedIn2.add(e2);
+						}
+					}
+				}
 			}
 		}
-
-		translatedEdgesIn1.retainAll(translatedEdgesIn2); 
-		int mappedEdges = translatedEdgesIn1.size();
-		int skippedEdges = totalNrEdges - groupedEdges - 2 * mappedEdges;
+		
+		int mappedEdges = mappedIn1.size() +  mappedIn2.size();
+		int groupedEdges = groupedIn1.size() +  groupedIn2.size();
+		int skippedEdges = totalNrEdges - groupedEdges - mappedEdges;
 
 		this.distance = skippedVertices + skippedEdges;
 	}
